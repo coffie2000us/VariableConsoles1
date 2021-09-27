84158cc78cf0cb28caf307130fa8e631ea0392f5
coffie2000us <charlescoffie.cri@gmail.com>
2021-09-27 14:10:30 -0400

    initial commit

---
+++ PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PlayingWithVariablesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Decleration
+            string name;
+            int age;
+            char myCharacter;
+            bool isHealthy;
+            double d;
+            decimal myMoney;
+
+            // Variable initialization
+            name = "Charles";
+            age = 36;
+            myCharacter = 'G';
+            isHealthy = true;
+            d = 3D;
+            myMoney = 3.5m;
+            Console.WriteLine($"My dog's name is Ralph, He is 10 years old");
+        }
+    }
+}
